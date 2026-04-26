4646277859dd61437a366dc89cc0ee610e9805c6
naziha <alaouchichenaziha@gmail.com>
2026-04-26 23:00:33 +0200

    .

---
+++ Medical_Clinic_Appointment/src/Medical_clinic/app_interface.java
@@ -0,0 +1,43 @@
+package Medical_clinic;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class app_interface {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					app_interface window = new app_interface();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public app_interface() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
