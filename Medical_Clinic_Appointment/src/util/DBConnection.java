4a1efed8597644ce35c1e4aae8b09420fe261dfb
nour-m29 <malak.sali0123@gmail.com>
2026-04-27 21:03:12 +0100

    final

--- Medical_Clinic_Appointment/src/util/DBConnection.java
+++ Medical_Clinic_Appointment/src/util/DBConnection.java
@@ -4,16 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    // Utilise les mêmes infos que dans ton ConnectionBD
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
     private static final String USER = "system"; 
     private static final String PASSWORD = "nour"; 
 
     public static Connection getConnection() {
         try {
-            
             Class.forName("oracle.jdbc.OracleDriver");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+            
+            // --- CETTE LIGNE EST LA SOLUTION ---
+            if (con != null) {
+                con.setAutoCommit(true); 
+            }
+            // ------------------------------------
+            
+            return con;
         } catch (Exception e) {
             System.err.println("Erreur de connexion : " + e.getMessage());
             return null;
